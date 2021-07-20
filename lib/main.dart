900afaefce5329ce0f8971481a6c77b3aab8da39
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2021-07-20 23:13:37 +0100

    Init bottom nav

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,37 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:knexxt/components/MyNavBar.dart';
+
+import 'components/Profile.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+
+  void selectedMenu(i) {
+    setState(() {
+      selectedIndex = i;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
       title: 'Knexxt',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('KNexxt'),
-          backgroundColor: (),
-        ),
-        body: Center(
-          child: Text('Welcome to KNexxt splash'),
-        ),
-      ),
+          body: Profile(),
+          bottomNavigationBar: MyNavBar(
+            selected: selectedIndex,
+            tap: selectedMenu,
+          )),
     );
   }
 }
