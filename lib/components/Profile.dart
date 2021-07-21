3bb71836df13aaa5950a0ef75149a60862060588
Oyinkansolight <oyinkansolababatunde30@gmail.com>
2021-07-21 15:05:34 +0100

    Profile layout complete

--- lib/components/Profile.dart
+++ lib/components/Profile.dart
@@ -8,58 +8,80 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Center(
-                    child: SvgPicture.network(
-                        'https://raw.githubusercontent.com/dnfield/flutter_svg/master/example/assets/deborah_ufw/new-camera.svg',
-                        placeholderBuilder: (BuildContext context) =>
-                            Container(child: const CircularProgressIndicator()),
-                        semanticsLabel: 'Location SVG',
-                        height: 300,
-                        width: 300,
-                        color: Colors.amber)),
-                ElevatedButton(
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  child: Text('Get Current Location'),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  maxLines: 8,
-                  // initialValue: 'More about your location...',
-                  decoration: InputDecoration(
-                      labelText: 'More about your location...',
-                      helperText: 'Specifics of your surroundings',
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.green)),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(4.0)))),
-                ),
-                SizedBox(height: 40),
-                Center(
-                    child: Text(
-                        'Another SVG for optional upload of images/videos')),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
+    return SingleChildScrollView(
+      child: Center(
+          child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                      child: SvgPicture.asset(
+                    'assets/Address-bro.svg',
+                    placeholderBuilder: (BuildContext context) =>
+                        Container(child: const CircularProgressIndicator()),
+                    semanticsLabel: 'Location SVG',
+                    height: 300,
+                    width: 300,
+                  )),
+                  ElevatedButton(
                     onPressed: () {
-                      //Update profile
+                      // Respond to button press
                     },
-                    child: Text('UPDATE')),
-                TextField()
-              ],
-            ),
-            padding: const EdgeInsets.fromLTRB(20, 0, 20, 10)));
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: Text('Get Current Location'),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    maxLines: 8,
+                    // initialValue: 'More about your location...',
+                    decoration: InputDecoration(
+                        labelText: 'More about your location...',
+                        helperText: 'Specifics of your surroundings',
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green)),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(4.0)))),
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                      child: SvgPicture.asset(
+                    'assets/upload.svg',
+                    placeholderBuilder: (BuildContext context) =>
+                        Container(child: const CircularProgressIndicator()),
+                    semanticsLabel: 'Upload SVG',
+                    height: 100,
+                    width: 100,
+                  )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                      child: Text('Video or Picture Upload',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        //Update profile
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                        child: Text('UPDATE'),
+                      )),
+                ],
+              ),
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 10))),
+    );
   }
 }
